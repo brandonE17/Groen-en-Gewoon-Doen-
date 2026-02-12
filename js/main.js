f8daeb679ff9c79e1740813a593da23b6fbdbbe5
Tijn1806 <Tijn@kromjong.nl>
2026-02-12 09:29:26 +0100

    Verbeterde versie

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
 
+    const modal = document.getElementById("loginModal");
+    const btn = document.getElementById("loginBtn");
+    const closeBtn = document.querySelector(".close");
+
+    btn.addEventListener("click", function () {
+        modal.style.display = "block";
+    });
+
+    closeBtn.addEventListener("click", function () {
+        modal.style.display = "none";
+    });
+
+    window.addEventListener("click", function (event) {
+        if (event.target === modal) {
+            modal.style.display = "none";
+        }
+    });
+
+});
